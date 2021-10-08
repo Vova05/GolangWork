07ad021e16730a761d475f52f75e0f1d1b8b8567
VovaGlh <viggiv01@gmail.com>
2021-10-09 00:07:02 +0300

    Добавил CSS JS файлы Добавилась защита

--- main/main.go
+++ main/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
+
 	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
 	// маршрутизаторе servemux
 	mux := http.NewServeMux()
@@ -13,7 +15,55 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Инициализируем FileServer, он будет обрабатывать
+	// HTTP-запросы к статическим файлам из папки "./ui/static".
+	// Обратите внимание, что переданный в функцию http.Dir путь
+	// является относительным корневой папке проекта
+	//fileServer := http.FileServer(http.Dir("GolangWork/ui/static/"))
+
+	// Используем функцию mux.Handle() для регистрации обработчика для
+	// всех запросов, которые начинаются с "/static/". Мы убираем
+	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	//mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	//log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	//err := http.ListenAndServe(":4000", mux)
+	//log.Fatal(err)
+
+
+	//mux := http.NewServeMux()
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("GolangWork/ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
